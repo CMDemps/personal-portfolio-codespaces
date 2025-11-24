054d0e920046216b15ad724c50c1904c011206b0
Clayton Demps <clayton.demps@outlook.com>
2025-11-24 21:18:33 +0000

    Updated portfolio images and about me section.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,25 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/codespace-background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "abstract background with code and futuristic interface elements";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am an IT professional with a background in security engineering. I love to solve problems by leveraging technology and creative thinking to drive innovation and make tech accessible and secure to all.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Security engineering",
+  "Cloud Architecture",
+  "Secure DevOps",
+  "Threat Detection",
 ];
 
 /**
@@ -44,8 +42,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "“Strive not to be a success, but rather to be of value.” – Albert Einstein";
 
 const About = () => {
   return (

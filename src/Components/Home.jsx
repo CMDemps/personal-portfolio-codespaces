867f6f204d6f9c24997928dc646f47e011b5e4e3
Clayton Demps <clayton.demps@outlook.com>
2025-11-25 13:49:33 +0000

    Updated file extension for headshot image import

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/UKY Headshot.jpg";
+import image from "../images/UKY Headshot.png";
 
 const imageAltText = "Headshot of Clayton Demps";
 const Home = ({ name, title }) => {
